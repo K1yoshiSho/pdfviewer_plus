2e8fcb0ca4ff4dbcf178dd7e30ad1e36ec60071c
Yelaman Yelmuratov <e99xxx@gmail.com>
2023-06-27 11:20:56 +0600

    upd: platform interface

--- lib/src/common/pdfviewer_plugin.dart
+++ lib/src/common/pdfviewer_plugin.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:syncfusion_pdfviewer_platform_interface/pdfviewer_platform_interface.dart';
 import 'package:uuid/uuid.dart';
 
+import '../platform_interface/pdfviewer_platform_interface.dart';
 import 'pdfviewer_helper.dart';
 
 /// Establishes communication between native(Android and iOS) code
@@ -23,23 +23,20 @@ class PdfViewerPlugin {
   /// Initialize the PDF renderer.
   Future<int> initializePdfRenderer(Uint8List documentBytes) async {
     _documentID = const Uuid().v1();
-    final String? pageCount = await PdfViewerPlatform.instance
-        .initializePdfRenderer(documentBytes, _documentID!);
+    final String? pageCount = await PdfViewerPlatform.instance.initializePdfRenderer(documentBytes, _documentID!);
     _pageCount = int.parse(pageCount!);
     return _pageCount;
   }
 
   /// Retrieves original height of PDF pages.
   Future<List<dynamic>?> getPagesHeight() async {
-    _originalHeight =
-        await PdfViewerPlatform.instance.getPagesHeight(_documentID!);
+    _originalHeight = await PdfViewerPlatform.instance.getPagesHeight(_documentID!);
     return _originalHeight;
   }
 
   /// Retrieves original width of PDF pages.
   Future<List<dynamic>?> getPagesWidth() async {
-    _originalWidth =
-        await PdfViewerPlatform.instance.getPagesWidth(_documentID!);
+    _originalWidth = await PdfViewerPlatform.instance.getPagesWidth(_documentID!);
     return _originalWidth;
   }
 
@@ -47,9 +44,7 @@ class PdfViewerPlugin {
     bool hasImage = false;
     _renderingPages?.forEach((RenderedImage image) {
       if ((image.pageIndex == pageIndex && !considerScale) ||
-          (image.pageIndex == pageIndex &&
-              considerScale &&
-              image.scale == scale)) {
+          (image.pageIndex == pageIndex && considerScale && image.scale == scale)) {
         hasImage = true;
       }
     });
@@ -57,26 +52,21 @@ class PdfViewerPlugin {
   }
 
   /// Get the specific image of PDF
-  Future<Map<int, List<dynamic>>?> _getImage(int pageIndex, double currentScale,
-      bool isZoomChanged, int currentPageNumber) async {
+  Future<Map<int, List<dynamic>>?> _getImage(
+      int pageIndex, double currentScale, bool isZoomChanged, int currentPageNumber) async {
     if (_renderingPages != null &&
         (!_hasRenderedImage(pageIndex, currentScale, false) ||
-            (isZoomChanged &&
-                pageIndex == currentPageNumber &&
-                !_hasRenderedImage(pageIndex, currentScale, true)))) {
-      final RenderedImage renderedImage =
-          RenderedImage(pageIndex, currentScale);
+            (isZoomChanged && pageIndex == currentPageNumber && !_hasRenderedImage(pageIndex, currentScale, true)))) {
+      final RenderedImage renderedImage = RenderedImage(pageIndex, currentScale);
       _renderingPages!.add(renderedImage);
       _nativeImage = CancelableOperation<Uint8List?>.fromFuture(
-          PdfViewerPlatform.instance
-              .getImage(pageIndex, currentScale, _documentID!));
+          PdfViewerPlatform.instance.getImage(pageIndex, currentScale, _documentID!));
       Future<Uint8List?> imageFuture = _nativeImage!.value.whenComplete(() {
         _renderingPages?.remove(renderedImage);
       });
       if (!kIsDesktop) {
         final Future<Uint8List?> image = imageFuture;
-        imageFuture = imageFuture.timeout(const Duration(milliseconds: 3000),
-            onTimeout: () {
+        imageFuture = imageFuture.timeout(const Duration(milliseconds: 3000), onTimeout: () {
           _renderingPages?.remove(renderedImage);
           return null;
         });
@@ -95,34 +85,22 @@ class PdfViewerPlugin {
   }
 
   ///  Retrieves PDF pages as image collection for specified pages.
-  Future<Map<int, List<dynamic>>?> getSpecificPages(
-      int startPageIndex,
-      int endPageIndex,
-      double currentScale,
-      bool isZoomChanged,
-      int currentPageNumber,
-      bool canRenderImage) async {
+  Future<Map<int, List<dynamic>>?> getSpecificPages(int startPageIndex, int endPageIndex, double currentScale,
+      bool isZoomChanged, int currentPageNumber, bool canRenderImage) async {
     imageCache.clear();
     if (!canRenderImage) {
       _nativeImage?.cancel();
       _renderingPages?.clear();
       return _renderedPages;
     }
-    for (int pageIndex = startPageIndex;
-        pageIndex <= endPageIndex;
-        pageIndex++) {
+    for (int pageIndex = startPageIndex; pageIndex <= endPageIndex; pageIndex++) {
       if (_renderedPages != null &&
           (!_renderedPages!.containsKey(pageIndex) ||
-              (isZoomChanged &&
-                  pageIndex == currentPageNumber &&
-                  currentScale > 1.75))) {
+              (isZoomChanged && pageIndex == currentPageNumber && currentScale > 1.75))) {
         currentScale = (pageIndex == currentPageNumber) ? currentScale : 1.0;
-        if (_pageScale != null &&
-            (!_pageScale!.containsKey(pageIndex) ||
-                _pageScale![pageIndex] != currentScale)) {
+        if (_pageScale != null && (!_pageScale!.containsKey(pageIndex) || _pageScale![pageIndex] != currentScale)) {
           _pageScale![pageIndex] = currentScale;
-          await _getImage(
-              pageIndex, currentScale, isZoomChanged, currentPageNumber);
+          await _getImage(pageIndex, currentScale, isZoomChanged, currentPageNumber);
         }
       }
     }
---
+++ lib/src/platform_interface/pdfviewer_platform_interface.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: strict_raw_type, always_specify_types, duplicate_ignore
+
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'method_channel_pdfviewer.dart';
+
+/// The interface that implementations of syncfusion_flutter_pdfviewer must implement.
+///
+/// Platform implementations should extend this class rather than implement it as `syncfusion_flutter_pdfviewer`
+/// does not consider newly added methods to be breaking changes. Extending this class
+/// (using `extends`) ensures that the subclass will get the default implementation, while
+/// platform implementations that `implements` this interface will be broken by newly added
+/// [PdfViewerPlatform] methods
+abstract class PdfViewerPlatform extends PlatformInterface {
+  /// Constructs a PdfViewerPlatform.
+  PdfViewerPlatform() : super(token: _token);
+  static PdfViewerPlatform _instance = MethodChannelPdfViewer();
+
+  static final Object _token = Object();
+
+  /// The default instance of [PdfViewerPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelPdfViewer]
+  static PdfViewerPlatform get instance => _instance;
+
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [PdfViewerPlatform] when they register themselves.
+  static set instance(PdfViewerPlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  /// Initializes the PDF renderer instance in respective platform by loading the PDF from the specified path.
+  ///
+  /// If success, returns page count else returns error message from respective platform
+  Future<String?> initializePdfRenderer(Uint8List documentBytes, String documentID) async {
+    throw UnimplementedError('initializePdfRenderer() has not been implemented.');
+  }
+
+  /// Gets the height of all pages in the document.
+  Future<List?> getPagesHeight(String documentID) async {
+    throw UnimplementedError('getPagesHeight() has not been implemented.');
+  }
+
+  /// Gets the width of all pages in the document.
+  Future<List?> getPagesWidth(String documentID) async {
+    throw UnimplementedError('getPagesWidth() has not been implemented.');
+  }
+
+  /// Gets the image's bytes information of the specified page.
+  Future<Uint8List?> getImage(int pageNumber, double scale, String documentID) async {
+    throw UnimplementedError('getImage() has not been implemented.');
+  }
+
+  /// Closes the PDF document.
+  Future<void> closeDocument(String documentID) async {
+    throw UnimplementedError('closeDocument() has not been implemented.');
+  }
+}
