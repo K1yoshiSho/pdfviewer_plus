641ee1d2d7fa97f91b8cf0f56fd93b900305bbc3
Yelaman Yelmuratov <e99xxx@gmail.com>
2023-06-27 18:09:47 +0600

    upd: swift file

--- ios/Classes/SyncfusionFlutterPdfViewerPlugin.m
+++ ios/Classes/SyncfusionFlutterPdfViewerPlugin.m
@@ -1,11 +1,11 @@
 #import "SyncfusionFlutterPdfViewerPlugin.h"
-#if __has_include(<syncfusion_flutter_pdfviewer/syncfusion_flutter_pdfviewer-Swift.h>)
-#import <syncfusion_flutter_pdfviewer/syncfusion_flutter_pdfviewer-Swift.h>
+#if __has_include(<pdfviewer_plus/pdfviewer_plus-Swift.h>)
+#import <pdfviewer_plus/pdfviewer_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "syncfusion_flutter_pdfviewer-Swift.h"
+#import "pdfviewer_plus-Swift.h"
 #endif
 
 @implementation SyncfusionFlutterPdfViewerPlugin
